c77b452b541fd1bc900bc79370e27d17bacdde11
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-07-27 18:43:21 +0200

    Implement endpoints for managing books:GET all, GET by Id and POST new books

--- src/index.js
+++ src/index.js
@@ -33,16 +33,81 @@ const getConnection = async () => {
   return connection;
 };
 
+// DEFINIR ENDPOINTS
+
+//Endpoint para obtener todos los libros
+server.get("/api/books", async (req, res) => {
+  try {
+    const conn = await getConnection();
+    const queryGetBooks = "SELECT * FROM books;";
+    const [results] = await conn.query(queryGetBooks);
+    conn.end();
+    res.json({ info: { count: results.length }, results });
+  } catch (error) {
+    res.json({ success: false, error: "UPS! ha ocurrido un error" });
+  }
+});
+
+//Endpoint para obtener un libro por si ID
+server.get("/api/books/:id", async (req, res) => {
+  try {
+    const conn = await getConnection();
+    const queryGetBook = "SELECT * FROM books WHERE id= ?;";
+    const [book] = await conn.query(queryGetBook, [req.params.id]);
+    conn.end();
+
+    if (book.length === 0) {
+      res.status(404).json({ error: "Oh! No hemos encontrado tu libro" });
+      return;
+    }
+    res.json(book[0]);
+  } catch (error) {
+    res.json({ success: false, error: "UPS! ha ocurrido un error" });
+  }
+});
+
+//Endpoint para agregar un nuevo libro
+server.post("/api/books", async (req, res) => {
+  try {
+    if (
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.genre ||
+      !req.body.synopsis
+    ) {
+      res.status(400).json({
+        success: false,
+        message: "¡HEY! Todos los campos son obligatorios",
+      });
+      return;
+    }
+    const conn = await getConnection();
+    const insertBook =
+      "INSERT INTO books (title, author, genre, synopsis) VALUES (?,?,?,?)";
+    const [insertResult] = await conn.execute(insertBook, [
+      req.body.title,
+      req.body.author,
+      req.body.genre,
+      req.body.synopsis,
+    ]);
+    conn.end();
+    res.json({
+      success: true,
+      id: insertResult.insertId,
+      message: "¡Bien! Nuevo libro",
+    });
+  } catch (error) {
+    res.json({ success: false, error: "UPS! ha ocurrido un error" });
+  }
+});
+// DEFINIR SERVIDORES ESTÁTICOS
+// DEFINIR PÁGINA 404
+
+server.get("*", function (req, res) {
+  res.status(404).send("Página no encontrada");
+});
+
 // ARRANCAR
 server.listen(port, () => {
   console.log(`Servidor iniciado escuchando en http://localhost:${port}`);
 });
-
-// DEFINIR ENDPOINTS
-
-server.get("/", function (req, res) {
-  res.send("Hello World!");
-});
-
-// DEFINIR SERVIDORES ESTÁTICOS
-// DEFINIR PÁGINA 404
